f0037eff789ccec1d5ec11e3cb41bb12ff4d8717
rems <rems@gmail.com>
2025-06-18 19:41:16 -0400

    feat: test should not passed

--- tests/test_my_class.py
+++ tests/test_my_class.py
@@ -2,7 +2,7 @@ from app.my_class import MyCalculator
 
 def test_add():
     calc = MyCalculator()
-    assert calc.add(2, 3) == 5
+    assert calc.add(2, 3) == 6
 
 def test_subtract():
     calc = MyCalculator()
