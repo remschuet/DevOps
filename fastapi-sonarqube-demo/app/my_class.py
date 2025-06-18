735fe8c9f975be75a9b11ae69e2fab66ad95efc2
rems <rems@gmail.com>
2025-06-18 18:43:33 -0400

    feat: start project

---
+++ fastapi-sonarqube-demo/app/my_class.py
@@ -0,0 +1,6 @@
+class MyCalculator:
+    def add(self, a: int, b: int) -> int:
+        return a + b
+
+    def subtract(self, a: int, b: int) -> int:
+        return a - b
---
+++ fastapi-sonarqube-demo/tests/test_my_class.py
@@ -0,0 +1,9 @@
+from app.my_class import MyCalculator
+
+def test_add():
+    calc = MyCalculator()
+    assert calc.add(2, 3) == 5
+
+def test_subtract():
+    calc = MyCalculator()
+    assert calc.subtract(5, 2) == 3
